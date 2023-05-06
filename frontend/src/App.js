75de7be289bcca69984dc16c44d94253975817a1
Sly <slymasterfunk@gmail.com>
2023-05-06 18:17:37 +0900

    Starting the Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <h1>Welocme To ProShop</h1>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
